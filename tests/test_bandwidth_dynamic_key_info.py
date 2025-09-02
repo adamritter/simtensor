f4eafec24f7c72543a474f4b74563639db4ab8b5
adamritter <58403584+adamritter@users.noreply.github.com>
2025-09-03 00:58:40 +0200

    bandwidth_dynamic: simplify short key

--- tests/test_bandwidth_dynamic_key_info.py
+++ tests/test_bandwidth_dynamic_key_info.py
@@ -5,10 +5,8 @@ from bandwidth_dynamic import _dp_short_key
 class TestShortKey(unittest.TestCase):
     def test_basic_key(self):
         key = ((2, 3), 0, (3, 4), 0, (2, 4), 0)
-        first_in, out, n = _dp_short_key(key)
-        self.assertEqual(first_in, ((2, 3), 0))
-        self.assertEqual(out, ((2, 4), 0))
-        self.assertEqual(n, 2)
+        short = _dp_short_key(key)
+        self.assertEqual(short, (2, 3, 0))
 
     def test_requires_two_inputs(self):
         key = ((4, 4), 0, (4, 4), 0)
